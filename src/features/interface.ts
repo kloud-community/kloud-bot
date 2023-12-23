465db4533b283711f136f03abe18894b0e071531
aqyuki <yksk171118@gmail.com>
2023-12-23 11:57:15 +0900

    feat: add interface

--- src/features/interface.ts
+++ src/features/interface.ts
@@ -1,4 +1,5 @@
-import type { AllEvents, Event } from "~/events/type";
+import type { AllEvents } from "~/events/type";
+import type { Client } from "discord.js";
 
 /**
  * ひとつひとつの機能のインターフェースです。
@@ -12,4 +13,19 @@ export interface Feature {
    * @param event 受信したイベントハンドラ。{@link Event}、または {@link AllEvents} を参照して下さい。
    */
   onEvent(event: AllEvents): Promise<void>;
+}
+
+/**
+ * Bot起動時に初期化される機能が実装するべきインターフェイスをです。
+ *
+ * Slash Commandの登録など、Bot起動時に行いたい処理を記入してください。
+ */
+export interface Initializer {
+  /**
+   * Bot起動時に実行される処理です。
+   * このメソッドは、{@link Client}が準備完了した際に呼び出されます。
+   *
+   * @param client Discord.jsのBotクラアントです。 {@link Client}を参照してください。
+   */
+  onReady(client: Client): Promise<void>;
 }
